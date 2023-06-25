a7251b022e1fb60586e58feb23cac1e23f20e89d
Alan <alan2cedeno3@gmail.com>
2023-06-25 10:56:41 -0400

    Quick Update
    Just testing Github to upload file to GitHub

---
+++ Functions&Variables.py
@@ -0,0 +1,20 @@
+# Ask user for their name
+name = input("What's your name? ").strip().title()
+
+# Remove Whitespace from String and Capitalize it
+
+# name = name.strip().title()
+
+#Split User's name into first and last name
+
+first, last = name.split()
+
+
+#Say Hello to User 
+
+print(f"Hello, {first}")
+
+"""
+This is a multi line comemnt
+You SEE!
+"""
