ee3bab806f6f1c6e606ab07a5d7faff288f5c5f9
Alan <alan2cedeno3@gmail.com>
2023-06-25 11:44:59 -0400

    L1 -> Finished Version
    L1 - Finished Version of the class which was Class 1

--- Functions&Variables.py
+++ Functions&Variables.py
@@ -1,20 +1,41 @@
-# Ask user for their name
-name = input("What's your name? ").strip().title()
+#( -----Ask user for their name-------)
 
-# Remove Whitespace from String and Capitalize it
+# name = input("What's your name? ").strip().title()
+
+# (------Remove Whitespace from String and Capitalize it-------)
 
 # name = name.strip().title()
 
-#Split User's name into first and last name
+#(--------Split User's name into first and last name--------)
 
-first, last = name.split()
+# first, last = name.split()
 
+#(---------Say Hello to User---------)
 
-#Say Hello to User 
-
-print(f"Hello, {first}")
+# print(f"Hello, {first}")
 
 """
 This is a multi line comemnt
 You SEE!
 """
+
+#(--------Defining a Function to Say Hello World and create a Paramter and return function with Name--------)
+
+# def hello(to = "world"):
+    # print("hello,", to)
+    
+# hello()
+# name = input("What's your Name? ")
+# hello(name)
+
+
+# (-------Defining a Function to Say Hello World by Defining Main and Def Hello to do the same thing as above---------)
+
+def main(): 
+    name = input("What's your Name? ")
+    hello(name)
+
+def hello(to = "world"):
+    print("hello,", to)
+
+main()
