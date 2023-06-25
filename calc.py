9eb811f4a73b7b2d231db5c9ee43f5c791f225c9
Alan <alan2cedeno3@gmail.com>
2023-06-25 11:44:24 -0400

    L1 - > Finished Version
    Lecture 1 ---- Finished Class Notes of Calc.Py

--- calc.py
+++ calc.py
@@ -1,23 +1,50 @@
-x = float(input("What's x? "))
-y = float(input("What's y? "))
+#  x = float(input("What's x? "))
+#  y = float(input("What's y? "))
 
-# To Round Z to Nearest Int
+# (---------To Round Z to Nearest Int-----------)
 
 #z = round(x + y)
 
-# Divide X and Y and Round it with the built in Operator
+#(-------- Divide X and Y and Round it with the built in Operator-----------)
 
 # z =round(x / y, 2 )
 
-# Dividing X and Y but a different approach
+#(-------------------- Dividing X and Y but a different approach------------)
 
-z = (x / y)
+# z = (x / y)
 
+#( --------- Same Rounding thing but with floats to the second decimal place------)
 
-print()
+# print(f"{z:.2f}")
 
 
 #To change to US Numbering System aka use commas
 # print(f"{z:,}")
 
-print(z)
+# print(z)
+
+# (--------------------Return Values---------------)
+
+def main():
+    x = int(input("What's x? "))
+    print("x squared is", square (x))
+    
+# (-----One way to square it----)
+
+# def square(n):
+    #return n ** 2
+
+# ** means to raise to power to
+  
+# (-----Another way to square it-----)
+  
+# def square(n):
+    # return n * n
+
+# (----------- Last way to Square it--------)
+
+def square(n):
+    return pow(n,2)
+# First Number is the number being raised to the power of and the second is to what power
+
+main()
