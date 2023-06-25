536374d410fe4f9cd21fd4677183c2e40f342fff
Alan <alan2cedeno3@gmail.com>
2023-06-25 10:53:53 -0400

    Quick Update
    Just testing Github and how to upload a file to Github

---
+++ calc.py
@@ -0,0 +1,23 @@
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+
+# To Round Z to Nearest Int
+
+#z = round(x + y)
+
+# Divide X and Y and Round it with the built in Operator
+
+# z =round(x / y, 2 )
+
+# Dividing X and Y but a different approach
+
+z = (x / y)
+
+
+print()
+
+
+#To change to US Numbering System aka use commas
+# print(f"{z:,}")
+
+print(z)
