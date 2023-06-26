bfa63f441633db99ec4ba8efb61403a5b900e00b
Alan <alan2cedeno3@gmail.com>
2023-06-25 23:14:04 -0400

    Creating Problem Set 0
    Finished 1 - 2 of PS0 have to Finish the rest of it

---
+++ PS0.py
@@ -0,0 +1,30 @@
+# (----------- Indoor Voice --------------)
+
+# InnerVoice = input(" Type Whatever you want! ").lower()
+
+# print(InnerVoice)
+
+# Finished
+
+
+
+# (-------------- PlayBack Speed ----------)
+
+# PlayBack = input("Say a Sentence. We are going to Slow it Down for you. ").replace(" ", "...")
+
+# print(PlayBack)
+
+# Finished
+
+
+
+# (--------------- Making Faces ---------------)
+
+
+
+
+
+# (-------------- Einstein ----------------------)
+
+
+# (-----------  Tip Calc -------------------)
