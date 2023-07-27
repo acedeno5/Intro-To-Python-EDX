1e70c7c470de1128045870bbc6436fd1d628215d
Alan <alan2cedeno3@gmail.com>
2023-07-27 15:23:08 -0400

    PS1
    Finished first problem

---
+++ PS1.py
@@ -0,0 +1,32 @@
+# Problem Set 1 for Conditionals
+
+# (---------Deep Thought---------------)
+
+# Step 1. Get user input
+
+answer = input("What is the answer to the Great Question of Life,the Universe and Everything? ")
+
+# Step 2. Print Yes if the user inputs 42 or (case-insentively) forty-two or forty two
+
+if answer.strip() == "42":
+    print("Yes")
+elif answer.lower().strip() == "forty-two":
+    print("Yes")
+elif answer.lower().strip() == "forty two":
+    print("Yes")
+
+# Step 3. Otherwise ouput No.
+
+else: print("No")
+
+
+#(---------Home Federal Savings Bank-----------)
+
+
+# (--------File Extensions-----------------------)
+
+
+#(------------Math Interpreter---------------------)
+
+
+#(---------------Meal Time-----------------------)
