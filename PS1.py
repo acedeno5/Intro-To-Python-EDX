c964885f8b209b508dc51a219fe3e57fa138e9c9
Alan <alan2cedeno3@gmail.com>
2023-08-04 12:54:57 -0400

    Updated PS1
    Finished 2nd Problem on PS1

--- PS1.py
+++ PS1.py
@@ -4,24 +4,47 @@
 
 # Step 1. Get user input
 
-answer = input("What is the answer to the Great Question of Life,the Universe and Everything? ")
+# answer = input("What is the answer to the Great Question of Life,the Universe and Everything? ")
 
 # Step 2. Print Yes if the user inputs 42 or (case-insentively) forty-two or forty two
 
-if answer.strip() == "42":
-    print("Yes")
-elif answer.lower().strip() == "forty-two":
-    print("Yes")
-elif answer.lower().strip() == "forty two":
-    print("Yes")
+# if answer.strip() == "42":
+    # print("Yes")
+# elif answer.lower().strip() == "forty-two":
+    # print("Yes")
+# elif answer.lower().strip() == "forty two":
+   #  print("Yes")
 
 # Step 3. Otherwise ouput No.
 
-else: print("No")
+# else: print("No")
+
+#Finished
 
 
 #(---------Home Federal Savings Bank-----------)
 
+# Step 1 - Get user input
+
+answer = input("Greeting: ")
+
+new_answer = answer.lower().strip()
+
+# Step 2 - Check is the answer has 'hello'
+
+if 'hello' in new_answer:
+    print("$0")
+
+# Step 3 - Check if answer starts with 'h'
+elif 'h' == new_answer[0]:
+    print("$20")
+
+# Step 4- Otherwise $100
+
+else:
+    print("$100")
+    
+    #Finished
 
 # (--------File Extensions-----------------------)
 
