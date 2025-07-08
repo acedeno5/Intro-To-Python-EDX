587efacdd279f6e566b2aa9efa81b79b8181e48a
acedeno5 <69852437+acedeno5@users.noreply.github.com>
2025-07-08 11:59:55 -0400

    Finished the Exceptions Lecture Notes
    Just an Update to the exceptions and finished the lecture.

--- 5_Exceptions.py
+++ 5_Exceptions.py
@@ -1,2 +1,72 @@
 #(-------Exceptions--------)
 
+print("hello, world")
+
+x = int(input("What' s x?"))
+print(f"x is {x}")
+
+#----Step 1----
+try:
+    x = int(input("What' s x?"))
+except ValueError:
+    print("x is not an integer")
+else:
+    print(f"x is {x}")
+    
+
+#---- Step 2-----
+    
+    
+while True:
+    try:
+        x = int(input("What' s x?"))
+    except ValueError:
+        print("x is not an integer")
+    else:
+        break
+
+print(f"x is {x}")
+
+
+# Another way to do it
+while True:
+    try:
+        x = int(input("What' s x?"))
+        break
+    except ValueError:
+        print("x is not an integer")
+
+print(f"x is {x}")
+
+
+#---Step 3--
+
+
+def main():
+    x = get_int(" What's x? ")
+    print(f"x is {x}")
+
+""" def get_int():
+    while True:
+        try:
+            x = int(input("What' s x?"))
+        except ValueError:
+            print("x is not an integer")
+        else:
+            break
+    return x """
+    
+def get_int(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            pass # Means it won't say anything
+
+main()
+
+
+
+
+    
+
